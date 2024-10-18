8b4fc52abbd4633544f191db64cb7a0c27bca4d8
Eshal109 <eshal.kashif@stu.ocsb.ca>
2024-10-18 14:49:00 +0000

    Assignment 2 python code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,12 @@
+diameter = input()
+if diameter < 0:
+  print("reenter diameter, number must be grater than 0")
+elif diameter > 0: 
+  def calc(r):
+    r=d/2
+    print(r)
+    area = math.pi*r**2
+    cirumference = 2*math.pi*2
+    print(area)
+    print(cirumference)
 
